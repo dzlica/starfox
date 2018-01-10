6367ea21934e79fad07b65101f740a3417c37e1f
nezih <nezihcihan@hotmail.com>
2018-01-10 13:06:42 +0100

    gameview modified with stars

--- app/src/main/java/com/greenfox/lica/starfox/GameView.java
+++ app/src/main/java/com/greenfox/lica/starfox/GameView.java
@@ -8,44 +8,66 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.ArrayList;
+
 /**
  * Created by lica on 2018. 01. 10..
  */
 
 public class GameView extends SurfaceView implements Runnable {
+
     volatile boolean playing;
     private Thread gameThread = null;
     private Player player;
+
     private Paint paint;
     private Canvas canvas;
     private SurfaceHolder surfaceHolder;
+
+    private ArrayList<Star> stars = new
+            ArrayList<Star>();
 
    public GameView(Context context, int screenX, int screenY) {
         super(context);
         player = new Player(context, screenX, screenY);
         surfaceHolder = getHolder();
         paint = new Paint();
+
+       int starNums = 100;
+       for (int i = 0; i < starNums; i++) {
+           Star s  = new Star(screenX, screenY);
+           stars.add(s);
+       }
     }
 
     @Override
     public void run() {
         while (playing) {
             update();
-
             draw();
-            
             control();
         }
     }
 
     private void update() {
         player.update();
+        for (Star s : stars) {
+            s.update(player.getSpeed());
+        }
     }
 
     private void draw() {
         if (surfaceHolder.getSurface().isValid()) {
             canvas = surfaceHolder.lockCanvas();
             canvas.drawColor(Color.GREEN);
+
+            paint.setColor(Color.YELLOW);
+
+            for (Star s : stars) {
+                paint.setStrokeWidth(s.getStarWidth());
+                canvas.drawPoint(s.getX(), s.getY(), paint);
+            }
+
             canvas.drawBitmap(
                     player.getBitmap(),
                     player.getX(),

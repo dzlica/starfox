21c8f26bbd974e6ff500683c8b18826224876c7e
nezih <nezihcihan@hotmail.com>
2018-01-10 11:59:01 +0100

    booster added to player

--- app/src/main/java/com/greenfox/lica/starfox/Player.java
+++ app/src/main/java/com/greenfox/lica/starfox/Player.java
@@ -14,15 +14,53 @@ public class Player {
     private int y;
     private int speed = 0;
 
+    private boolean boosting;
+    private final int GRAVITY = -10;
+    private int maxY;
+    private int minY;
+    private final int MIN_SPEED = 1;
+    private final int MAX_SPEED = 20;
+
     public Player(Context context) {
         x = 75;
         y = 50;
         speed = 1;
         bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.player);
+        boosting = false;
+    }
+
+    public void setBoosting() {
+        boosting = true;
     }
 
     public void update(){
-        x++;
+        if (boosting) {
+            //speeding up the ship
+            speed += 2;
+        } else {
+            //slowing down if not boosting
+            speed -= 5;
+        }
+        //controlling the top speed
+        if (speed > MAX_SPEED) {
+            speed = MAX_SPEED;
+        }
+        //if the speed is less than min speed
+        //controlling it so that it won't stop completely
+        if (speed < MIN_SPEED) {
+            speed = MIN_SPEED;
+        }
+
+        //moving the ship down
+        y -= speed + GRAVITY;
+
+        //but controlling it also so that it won't go off the screen
+        if (y < minY) {
+            y = minY;
+        }
+        if (y > maxY) {
+            y = maxY;
+        }
     }
 
     public Bitmap getBitmap() {

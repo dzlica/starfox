e39185f086e6c723073a2222fab6fe621a646d57
Dzindzisz Magdalena <dzlica@gmail.com>
2018-01-10 11:58:13 +0100

    feat(draw) drawplayer to gameview, it moves

--- app/src/main/java/com/greenfox/lica/starfox/GameView.java
+++ app/src/main/java/com/greenfox/lica/starfox/GameView.java
@@ -1,7 +1,11 @@
 package com.greenfox.lica.starfox;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 /**
@@ -11,9 +15,16 @@ import android.view.SurfaceView;
 public class GameView extends SurfaceView implements Runnable {
     volatile boolean playing;
     private Thread gameThread = null;
+    private Player player;
+    private Paint paint;
+    private Canvas canvas;
+    private SurfaceHolder surfaceHolder;
 
     public GameView(Context context) {
         super(context);
+        player = new Player(context);
+        surfaceHolder = getHolder();
+        paint = new Paint();
     }
 
     @Override
@@ -28,10 +39,20 @@ public class GameView extends SurfaceView implements Runnable {
     }
 
     private void update() {
-
+        player.update();
     }
 
     private void draw() {
+        if (surfaceHolder.getSurface().isValid()) {
+            canvas = surfaceHolder.lockCanvas();
+            canvas.drawColor(Color.GREEN);
+            canvas.drawBitmap(
+                    player.getBitmap(),
+                    player.getX(),
+                    player.getY(),
+                    paint);
+            surfaceHolder.unlockCanvasAndPost(canvas);
+        }
 
     }
 
